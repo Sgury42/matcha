59083ca261024648a33a104cb643736c47c6263f
Sgury42 <sgury@student.42.fr>
2020-04-13 14:11:40 +0200

    ready to prod

---
+++ build/precache-manifest.81051b339174fbb7960d5fe2a10877bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0125beb393e2d2293d0604bf34e04d45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7adb9c5ac5bf0929a8cb",
+    "url": "/static/js/2.4cd72f8a.chunk.js"
+  },
+  {
+    "revision": "105db135bd46e9ecaf8c1e161716348c",
+    "url": "/static/js/2.4cd72f8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02b18610ece7fed1dbf1",
+    "url": "/static/js/main.5c027227.chunk.js"
+  },
+  {
+    "revision": "1308a0e703db5f65f204",
+    "url": "/static/js/runtime-main.678a9f41.js"
+  }
+]);
